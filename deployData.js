0e463461d4855e65e07d08f4f72254b57a313f09
Dinda Nurafni <dindanurafni205@gmail.com>
2023-06-28 12:36:00 +0700

    kedua berhasil

---
+++ deployData.js
@@ -0,0 +1,8 @@
+
+const studentName = 'Dinda Nurafni';
+
+const studentId = 'FE4573074'; // TODO: replace this
+
+const NetlifyDeployUrl = 'https://dindanurafni.netlify.app/'; // TODO: replace this
+
+module.exports = { studentName, studentId, NetlifyDeployUrl };
